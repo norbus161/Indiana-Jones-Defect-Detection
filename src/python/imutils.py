f37b9d67743f6f2b58361ecc28f3d163088f8f2a
Patrick Würflinger <wuerflingerp@gmx.at>
2021-01-28 19:30:25 +0100

    added some stuff

--- src/python/imutils.py
+++ src/python/imutils.py
@@ -1,6 +1,63 @@
 import cv2
 import numpy as np
 import math
+import io
+import os
+import scipy.misc
+import six
+import time
+import glob
+import matplotlib
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from IPython.display import display
+from six import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+from utils import ops as utils_ops
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+def load_image_into_numpy_array(path):
+  """Load an image from file into a numpy array. """
+    img_data = tf.io.gfile.GFile(path, 'rb').read()
+    image = Image.open(BytesIO(img_data))
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+
+
+def run_inference_for_single_image(model, image):
+      """Perform prediction on input image. """
+    category_index = label_map_util.create_category_index_from_labelmap("../../config/labelmap.pbtxt", use_display_name=True)
+    tf.keras.backend.clear_session()
+    image = np.asarray(image)
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis,...]
+
+    # Run inference
+    model_fn = model.signatures['serving_default']
+    output_dict = model_fn(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(output_dict.pop('num_detections'))
+    output_dict = {key:value[0, :num_detections].numpy() for key,value in output_dict.items()}
+    output_dict['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+   
+    # Handle models with masks:
+    if 'detection_masks' in output_dict:
+        # Reframe the the bbox mask to the image size.
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(output_dict['detection_masks'], output_dict['detection_boxes'], image.shape[0], image.shape[1])      
+        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.8, tf.uint8)
+        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+
+    return output_dict
 
 def im2double(img):
    """ Return a image in float64 format in a range of [0, 1] """
@@ -57,7 +114,7 @@ def imclearborder(imgBW, radius):
 
    return imgBWcopy
 
-#### bwareaopen definition
+# bwareaopen definition
 def bwareaopen(imgBW, areaPixels):
    """ Removes all connected components (objects) that have fewer than P pixels from the binary image BW, p
        roducing another binary image, BW2. This operation is known as an area opening. """
---
+++ src/python/inspection.py
@@ -0,0 +1,79 @@
+from sklearn.metrics import accuracy_score, confusion_matrix
+import cv2
+from matplotlib import pyplot as plt
+
+import imutils as utils
+import glob
+import initdata as init
+import random  # only used to return random labels
+
+# hook here your function to inspect image and return label for the detected defect
+
+
+def inspect_image(img, defects):
+    img_processed = img
+    predicted_label = random.randrange(0, 7)
+    return img_processed, predicted_label
+
+
+# read background image for shading correction
+imgbackground = cv2.imread('../../img/Other/image_100.jpg')
+
+template, defects = init.initdata()
+
+# Enable plotting of images which are processed
+do_plot = False  
+
+# container for ground truth label and predicted label
+y_true, y_pred = [], []  
+
+#  machine learning stuff
+category_index = label_map_util.create_category_index_from_labelmap(
+    labelmap_path, use_display_name=True)
+tf.keras.backend.clear_session()
+model = tf.saved_model.load('../../model/saved_model')
+
+
+for class_label, defect_type in enumerate(defects):
+    imageDir = "../../img/" + defects[defect_type]['dir']
+
+    # read all images from folders given in a list
+    for imagePath in glob.glob(imageDir + "*.jpg"):
+
+        img = cv2.imread(imagePath)
+        if img is None:
+            print("Error loading: " + imagePath)
+            # end this loop iteration and move on to next image
+            continue
+
+        """ ----------------------------------------------------- 
+        ... perform defect detection here """
+
+        # 1. compensate the background of the image
+        img = utils.shadding(img, imgbackground)
+
+        # 2. perform a call to the object detection network
+        prediction_dict = utils.run_inference_for_single_image(model, img)
+
+        # 3. analzye the prediction dictionary 
+        # if prediction_dict.
+
+        """ ----------------------------------------------------- """
+
+        img_processed, predicted_label = inspect_image(img, defects)
+        y_pred.append(predicted_label)
+        y_true.append(class_label)  # append real class label to true y's
+
+        if (do_plot):
+            f, (ax1, ax2) = plt.subplots(1, 2)
+            ax1.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            ax1.axis("off")
+            ax1.set_title(imagePath)
+            ax2.imshow(img_processed, cmap='gray')
+            ax2.axis("off")
+            ax2.set_title("Processed image")
+            plt.show()
+
+
+print("Accuracy: ", accuracy_score(y_true, y_pred))
+print("Confusion matrix:\n", confusion_matrix(y_true, y_pred))
